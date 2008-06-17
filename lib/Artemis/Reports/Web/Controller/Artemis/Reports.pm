2b8f68e4c77bc3a3aa50368d47e76e86ae47e19c
Steffen Schwigon <steffen.schwigon@amd.com>
2008-06-17 12:10:04 +0200

    - reports with progress/success bar, some cross linking - mason components for list of reports

--- lib/Artemis/Reports/Web/Controller/Artemis/Reports.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Reports.pm
@@ -2,6 +2,7 @@ package Artemis::Reports::Web::Controller::Artemis::Reports;
 
 use strict;
 use warnings;
+use diagnostics;
 
 use parent 'Catalyst::Controller::BindLex';
 __PACKAGE__->config->{bindlex}{Param} = sub { $_[0]->req->params };
@@ -9,6 +10,26 @@ __PACKAGE__->config->{bindlex}{Param} = sub { $_[0]->req->params };
 sub index :Path :Args(0)
 {
         my ( $self, $c ) = @_;
+}
+
+sub prepare_simple_reportlist : Private
+{
+        my ( $self, $c, $reports ) = @_;
+
+        my @simple_reportlist : Stash;
+        while (my $report = $reports->next)
+        {
+                my $r = {
+                         id                 => $report->id,
+                         suite_name         => $report->suite ? $report->suite->name : 'unknown',
+                         suite_id           => $report->suite ? $report->suite->id : '0',
+                         machine_name       => $report->machine_name || 'unknown',
+                         created_at_ymd_hms => $report->created_at->ymd('-')." ".$report->created_at->hms(':'),
+                         created_at_ymd     => $report->created_at->ymd('-'),
+                         success_ratio      => $report->success_ratio,
+                        };
+                push @simple_reportlist, $r;
+        }
 }
 
 1;
--- lib/Artemis/Reports/Web/Controller/Artemis/Reports/Suite.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Reports/Suite.pm
@@ -10,20 +10,13 @@ sub index :Path :Args(1)
 {
         my ( $self, $c, $suite_id ) = @_;
 
-        my @reports : Stash;
-        my $reports = $c->model('ReportsDB')->resultset('Report')->search({suite_id => $suite_id}, {order_by => 'id desc'});
+        my $reports = $c->model('ReportsDB')->resultset('Report')->search
+            (
+             { suite_id => $suite_id },
+             { order_by => 'id desc' },
+            );
+        $c->forward('/artemis/reports/prepare_simple_reportlist', [ $reports ]);
 
-        while (my $report = $reports->next)
-        {
-                my $r = {
-                         id           => $report->id,
-                         suite_name   => $report->suite ? $report->suite->name : 'unknown',
-                         suite_id     => $report->suite ? $report->suite->id : '0',
-                         machine_name => $report->machine_name || 'unknown',
-                         created_at   => $report->created_at->ymd('-')." ".$report->created_at->hms(':'),
-                        };
-                push @reports, $r;
-        }
 }
 
 1;
