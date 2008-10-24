f7a02f756343921db1940a4ef1934c7a14e4f91e
Steffen Schwigon <steffen.schwigon@amd.com>
2008-10-24 17:13:05 +0200

    - experiments to show filesizes, but deactivated

--- lib/Artemis/Reports/Web/Controller/Artemis/Reports/Id.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Reports/Id.pm
@@ -10,6 +10,17 @@ sub index :Path :Args(1)
         my ( $self, $c, $report_id ) = @_;
 
         my $report : Stash = $c->model('ReportsDB')->resultset('Report')->find($report_id);
+#         my $report : Stash;
+#         $report = ($c->model('ReportsDB')->resultset('Report')->search
+#                    (
+#                     { id => $report_id},
+#                     {
+#                      +select => [ \"length('filecontent')" ],               # " ]
+#                      +as     => [ "length" ],
+#                     },
+#                    )->all)[0];
+#         use Data::Dumper;
+#         print STDERR Dumper($report);
 }
 
 1;
