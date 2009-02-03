c69f4295b7649edd46313ac67d5686176a8c4a16
Steffen Schwigon <steffen.schwigon@amd.com>
2009-02-03 12:12:19 +0100

    - Web-GUI - prepare, show and highlight other reports of same group

--- lib/Artemis/Reports/Web/Controller/Artemis/Reports/Id.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Reports/Id.pm
@@ -9,7 +9,43 @@ sub index :Path :Args(1)
 {
         my ( $self, $c, $report_id ) = @_;
 
-        my $report : Stash = $c->model('ReportsDB')->resultset('Report')->find($report_id);
+        my $report         : Stash;
+        my $reportlist_rga : Stash = {};
+        my $reportlist_rgt : Stash = {};
+
+        $report = $c->model('ReportsDB')->resultset('Report')->find($report_id);
+
+        if (my $rga = $report->reportgrouparbitrary) {
+                #my $rga_reports = $c->model('ReportsDB')->resultset('ReportgroupArbitrary')->search ({ arbitrary_id => $rga->arbitrary_id });
+                my $rga_reports = $c->model('ReportsDB')->resultset('Report')->search
+                    (
+                     {
+                      "reportgrouparbitrary.arbitrary_id" => $rga->arbitrary_id
+                     },
+                     {  order_by  => 'id desc',
+                        join      => [ 'reportgrouparbitrary',              'reportgrouptestrun', ],
+                        '+select' => [ 'reportgrouparbitrary.arbitrary_id', 'reportgrouparbitrary.primaryreport', 'reportgrouptestrun.testrun_id', 'reportgrouptestrun.primaryreport' ],
+                        '+as'     => [ 'rga_id',                            'rga_primary',                        'rgt_id',                        'rgt_primary'                      ],
+                     }
+                    );
+                $reportlist_rga = $c->forward('/artemis/reports/prepare_simple_reportlist', [ $rga_reports ]);
+        }
+
+        if (my $rgt = $report->reportgrouptestrun) {
+                #my $rgt_reports = $c->model('ReportsDB')->resultset('ReportgroupTestrun')->search ({ testrun_id => $rgt->testrun_id });
+                my $rgt_reports = $c->model('ReportsDB')->resultset('Report')->search
+                    (
+                     {
+                      "reportgrouptestrun.testrun_id" => $rgt->testrun_id
+                     },
+                     {  order_by  => 'id desc',
+                        join      => [ 'reportgrouparbitrary',              'reportgrouptestrun', ],
+                        '+select' => [ 'reportgrouparbitrary.arbitrary_id', 'reportgrouparbitrary.primaryreport', 'reportgrouptestrun.testrun_id', 'reportgrouptestrun.primaryreport' ],
+                        '+as'     => [ 'rga_id',                            'rga_primary',                        'rgt_id',                        'rgt_primary'                      ],
+                     }
+                    );
+                $reportlist_rgt = $c->forward('/artemis/reports/prepare_simple_reportlist', [ $rgt_reports ]);
+        }
 }
 
 1;
