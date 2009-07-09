a4fe0d61e1d180470f8896bb4043ee1dab6d7781
Maik Hentsche <maik.hentsche@amd.com>
2009-07-09 10:48:37 +0200

    Web: fixed broken handling of empty reportfiles

--- lib/Artemis/Reports/Web/Controller/Artemis/ReportFile/Id.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/ReportFile/Id.pm
@@ -9,7 +9,7 @@ sub index :Path :Args(1)
         my ( $self, $c, $file_id ) = @_;
         my $reportfile : Stash = $c->model('ReportsDB')->resultset('ReportFile')->find($file_id);
 
-        if ($reportfile) {
+        if ($reportfile->filecontent) {
                 my $disposition = $reportfile->contenttype =~ /plain/ ? 'inline' : 'attachment';
                 $c->response->content_type ($reportfile->contenttype || 'application/octet-stream');
                 $c->response->header ("Content-Disposition" => $disposition.'; filename="'.$reportfile->filename.'"');
