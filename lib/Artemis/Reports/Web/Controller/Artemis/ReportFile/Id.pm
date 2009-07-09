9ec3036b2836f6bd0cc38e706c9863e9600ef8e2
Steffen Schwigon <steffen.schwigon@amd.com>
2009-07-09 16:25:09 +0200

    webgui: discriminate between empty and nonexistent files

--- lib/Artemis/Reports/Web/Controller/Artemis/ReportFile/Id.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/ReportFile/Id.pm
@@ -9,17 +9,25 @@ sub index :Path :Args(1)
         my ( $self, $c, $file_id ) = @_;
         my $reportfile : Stash = $c->model('ReportsDB')->resultset('ReportFile')->find($file_id);
 
-        if ($reportfile->filecontent) {
-                my $disposition = $reportfile->contenttype =~ /plain/ ? 'inline' : 'attachment';
-                $c->response->content_type ($reportfile->contenttype || 'application/octet-stream');
-                $c->response->header ("Content-Disposition" => $disposition.'; filename="'.$reportfile->filename.'"');
-                $c->response->body ($reportfile->filecontent);
-        } else {
+        if (not $reportfile)
+        {
                 $c->response->content_type ("plain");
                 $c->response->header ("Content-Disposition" => 'inline; filename="nonexistent.reportfile.'.$file_id.'"');
                 $c->response->body ("Error: File with id $file_id does not exist.");
         }
-
+        elsif (not $reportfile->filecontent)
+        {
+                $c->response->content_type ("plain");
+                $c->response->header ("Content-Disposition" => 'inline; filename="empty.reportfile.'.$file_id.'"');
+                $c->response->body ("Error: File with id $file_id is empty.");
+        }
+        else
+        {
+                my $disposition = $reportfile->contenttype =~ /plain/ ? 'inline' : 'attachment';
+                $c->response->content_type ($reportfile->contenttype || 'application/octet-stream');
+                $c->response->header ("Content-Disposition" => $disposition.'; filename="'.$reportfile->filename.'"');
+                $c->response->body ($reportfile->filecontent);
+        }
 }
 
 1;
