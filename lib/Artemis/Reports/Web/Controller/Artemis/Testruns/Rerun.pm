589dea7d779942b917710832af74128260391c82
Maik Hentsche <maik.hentsche@amd.com>
2009-08-04 11:30:23 +0200

    Web: added controller to restart testruns

---
+++ lib/Artemis/Reports/Web/Controller/Artemis/Testruns/Rerun.pm
@@ -0,0 +1,14 @@
+package Artemis::Reports::Web::Controller::Artemis::Testruns::Rerun;
+
+use strict;
+use warnings;
+
+use parent 'Artemis::Reports::Web::Controller::Base';
+
+sub index :Path :Args(1)
+{
+        
+}
+
+
+1;
