f79a45280299ace8b8e9feba983d03cf06142564
Maik Hentsche <maik.hentsche@amd.com>
2009-08-07 10:51:47 +0200

    Web: add check whether precondition exists to precondition_by_id controller

--- lib/Artemis/Reports/Web/Controller/Artemis/Preconditions/Id.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Preconditions/Id.pm
@@ -11,6 +11,10 @@ sub index :Path :Args(1)
         my $precondition : Stash;
         
         my $precond_search = $c->model('TestrunDB')->resultset('Precondition')->find($id);
+        if (not $precond_search) {
+                $c->response->body(qq(No precondition with id "$id" found in the database!));
+                return;
+        }
         $precondition = $precond_search->precondition_as_hash;
         $precondition->{id} = $precond_search->id;
         return;
