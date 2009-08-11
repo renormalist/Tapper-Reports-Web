e21f8fd9101aaf8a4a0bef3a5f2baf65cdda3907
Maik Hentsche <maik.hentsche@amd.com>
2009-08-11 14:25:43 +0200

    Web: added FormFu to parents of all controllers

--- lib/Artemis/Reports/Web/Controller/Base.pm
+++ lib/Artemis/Reports/Web/Controller/Base.pm
@@ -3,7 +3,7 @@ package Artemis::Reports::Web::Controller::Base;
 use strict;
 use warnings;
 
-use parent 'Catalyst::Controller::BindLex';
+use parent qw(Catalyst::Controller::HTML::FormFu Catalyst::Controller::BindLex);
 __PACKAGE__->config->{bindlex}{Param}    = sub { $_[0]->req->params };
 __PACKAGE__->config->{unsafe_bindlex_ok} = 1;
 
