fa9b2eb179fe24c8809b5d5728bc7ef75e7af32c
Maik Hentsche <maik.hentsche@amd.com>
2009-08-27 15:22:42 +0200

    Web: improved dist_upload script to include generation of distribution

--- script/dist_upload_wotan.sh
+++ script/dist_upload_wotan.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
+EXECDIR=$(dirname $0)
 DISTFILES='Artemis*-*.*.tar.gz '
+$EXECDIR/../../Artemis/scripts/artemis_version_increment.pl $EXECDIR/../lib/Artemis/Reports/Web.pm
+cd $EXECDIR/..
+
+if [[ -e MANIFEST ]]
+then
+  rm MANIFEST
+fi
+make manifest || exit -1
+
+perl Makefile.PL || exit -1
+make dist || exit -1
 
 # -----------------------------------------------------------------
 # It is important to not overwrite existing files.
@@ -16,4 +28,4 @@ rsync -vv --progress --ignore-existing ${DISTFILES} artemis@wotan:/home/artemis/
 echo ""
 echo '----- re-index -------------------------------------------------'
 ssh artemis@wotan /home/artemis/perl510/bin/cpansite -vl index /home/artemis/CPANSITE/CPAN/
-
+ssh artemis@wotan 'echo "/home/artemis/perl510/bin/cpansite install Artemis::Reports::Web" | bash -l'
