895d3a6d08f6bbdb1f81d206f02613af1cfef03d
Steffen Schwigon <steffen.schwigon@amd.com>
2010-01-06 17:18:36 +0100

    WebGUI: code cleanup

--- lib/Artemis/Reports/Web/Controller/Artemis/Testruns/Date.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis/Testruns/Date.pm
@@ -1,4 +1,4 @@
-package Artemis::Reports::Web::Controller::Artemis::Reports::Date;
+package Artemis::Reports::Web::Controller::Artemis::Testruns::Date;
 
 use 5.010;
 
@@ -13,8 +13,8 @@ sub index :Path :Args(1)
 {
         my ( $self, $c, $showdays ) = @_;
 
-        my $days             : Stash = $showdays || 7;
-        my $filter_condition : Stash = {};
+        my $days             : Stash = $showdays || 2;
+        my $filter_condition : Stash = { "me.id" => { '>=', 22530 } };
         $c->forward('/artemis/reports/prepare_this_weeks_reportlists');
 }
 
