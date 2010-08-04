838aab837fbe797d61e3450683a85afe06676cc0
Maik Hentsche <maik.hentsche@amd.com>
2010-08-04 16:07:04 +0200

    Web: [FEAT] Overview page
    With ethe new filter facility URLs like suite/all are no longer usable
    to get a list of all suites, especially since "all" is a valid name for
    a suite. Thus, I implemented a new overview page that gives an overview
    of (currently) all hosts or all suites and links to reports filtered by
    the associated elements. This page could easily be extended to contain
    e.g. users, dates or whatever.

---
+++ lib/Artemis/Reports/Web/Controller/Artemis/Overview.pm
@@ -0,0 +1,52 @@
+package Artemis::Reports::Web::Controller::Artemis::Overview;
+
+
+use parent 'Artemis::Reports::Web::Controller::Base';
+
+use common::sense;
+
+
+sub auto :Private
+{
+        my ( $self, $c ) = @_;
+        $c->forward('/artemis/overview/prepare_navi');
+}
+
+
+
+sub index :Path :Args()
+{
+        my ( $self, $c, $type ) = @_;
+        my $overviews : Stash;
+        given ($type){
+                when ('suite') {
+                        my $suite_rs = $c->model('ReportsDB')->resultset('Suite');
+                        $overviews = { map{$_->name, '/artemis/reports/suite/'.$_->id } $suite_rs->all };
+                }
+                when ('host')  {
+                        my $reports = $c->model('ReportsDB')->resultset('Report')->search({},
+                                                                                          { columns => [ qw/machine_name/ ],
+                                                                                            distinct => 1});
+                        $overviews = { map{$_->machine_name, '/artemis/reports/host/'.$_->machine_name} $reports->all };
+                }
+                default {
+                        $overviews = {
+                                      Testsuites => '/artemis/overview/suite',
+                                      Hostnames  => '/artemis/overview/host',
+                                     };
+                        
+                }
+        }
+}
+
+sub prepare_navi : Private
+{
+        my ( $self, $c ) = @_;
+
+        my $nav : Stash = {
+                           Suites => "/artemis/overview/suite",
+                           Hosts  => "/artemis/overview/host",
+                          };
+}
+
+1;
