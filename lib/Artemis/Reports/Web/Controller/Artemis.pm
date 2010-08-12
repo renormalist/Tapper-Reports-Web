e2132626a1d0370580c041a45145c7a6dad0b549
Maik Hentsche <maik.hentsche@amd.com>
2010-08-12 10:07:20 +0200

    Web: made overview to start page

--- lib/Artemis/Reports/Web/Controller/Artemis.pm
+++ lib/Artemis/Reports/Web/Controller/Artemis.pm
@@ -15,9 +15,9 @@ sub index :Path :Args(0)
         my $body = <<EOF;
 <html>
 <head>
-<meta http-equiv="refresh" content="0; URL=/artemis/reports">
-<meta name="description" content="Artemis Reports"
-<title>Artemis | Reports</title>
+<meta http-equiv="refresh" content="0; URL=/artemis/overview">
+<meta name="description" content="Artemis"
+<title>Artemis</title>
 </head>
 EOF
         $c->response->body($body);
