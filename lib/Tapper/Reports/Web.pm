c37673b265902141989e6c47c61842367b9f2486
Steffen Schwigon <steffen.schwigon@amd.com>
2011-02-08 15:42:29 +0100

    rename - src/Tapper-Reports-Web (basedir)

---
+++ lib/Tapper/Reports/Web.pm
@@ -0,0 +1,107 @@
+package Tapper::Reports::Web;
+
+use strict;
+use warnings;
+
+use 5.010;
+
+use Catalyst::Runtime '5.70';
+use Hash::Merge;
+
+use Class::C3::Adopt::NEXT;
+
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+#   ConfigLoader: will load the configuration from a Config::General file in the
+#                 application's home directory
+# Static::Simple: will serve static files from the application's root
+#                 directory
+
+use parent qw/Catalyst/;
+
+our $VERSION = '2.010149';
+
+# used by Catalyst::Plugin::ConfigLoader
+sub finalize_config
+{
+        my $c = shift;
+
+        $c->NEXT::ACTUAL::finalize_config;
+        my $env =
+            $ENV{HARNESS_ACTIVE}                 ? 'test'
+                : $ENV{TAPPER_REPORTS_WEB_LIVE} ? 'live'
+                    : 'development';
+        Hash::Merge::set_behavior('RIGHT_PRECEDENT');
+        $c->config(
+                   Hash::Merge::merge(
+                                      $c->config,
+                                      $c->config->{ $env } || {} ,
+                                     )
+                  );
+
+        return;
+}
+
+sub debug
+{
+        return $ENV{TAPPER_REPORTS_WEB_LIVE} || $ENV{HARNESS_ACTIVE} ? 0 : 1;
+}
+
+# I am sick of getting relocated/rebase on our local path!
+# Cut away a trailing 'tapper/' from base and prepend it to path.
+# All conditionally only when this annoying environment is there.
+sub prepare_path
+{
+        my $c = shift;
+
+        $c->NEXT::prepare_path(@_);
+
+        my $base        =  $c->req->{base}."";
+        $base           =~ s,tapper/$,, if $base;
+        $c->req->{base} =  bless( do{\(my $o = $base)}, 'URI::http' );
+        $c->req->path('tapper/'.$c->req->path) unless ( $c->req->path =~ m,^tapper/?,);
+}
+
+
+# Configure the application.
+__PACKAGE__->config( name => 'Tapper::Reports::Web' );
+__PACKAGE__->config->{static}->{dirs} = [
+                                         'tapper/static',
+                                        ];
+
+# Start the application
+__PACKAGE__->setup(qw/-Debug
+                      ConfigLoader
+                      Static::Simple Session
+                      Session::State::Cookie
+                      Session::Store::File/);
+
+
+=head1 NAME
+
+Tapper::Reports::Web - Catalyst based application
+
+=head1 SYNOPSIS
+
+    script/tapper_reports_web_server.pl
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 SEE ALSO
+
+L<Tapper::Reports::Web::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+Copyright 2008 OSRC SysInt Team, all rights reserved.
+
+=head1 LICENSE
+
+This program is released under the following license: proprietary
+
+=cut
+
+1;
---
+++ lib/Tapper/Reports/Web/Controller/Tapper/Reports.pm
@@ -0,0 +1,218 @@
+package Tapper::Reports::Web::Controller::Tapper::Reports;
+
+
+use parent 'Tapper::Reports::Web::Controller::Base';
+
+use DateTime::Format::Natural;
+use Data::Dumper;
+
+use Tapper::Reports::Web::Util::Filter;
+use Tapper::Reports::Web::Util::Report;
+use common::sense;
+## no critic (RequireUseStrict)
+
+sub auto :Private
+{
+        my ( $self, $c ) = @_;
+
+        $c->forward('/tapper/reports/prepare_navi');
+}
+
+sub index :Path :Args()
+{
+        my ( $self, $c, @args ) = @_;
+        my $error_msg : Stash;
+
+        exit 0 if $args[0] eq 'exit';
+
+        my $filter = Tapper::Reports::Web::Util::Filter->new(context => $c);
+        my $filter_condition = $filter->parse_filters(\@args);
+
+        if ($filter_condition->{error}) {
+                $error_msg = join("; ", @{$filter_condition->{error}});
+                $c->res->redirect("/tapper/reports/days/2");
+
+        }
+
+        my $requested_day : Stash = 
+          $filter->requested_day || DateTime::Format::Natural->new->parse_datetime("today at midnight");
+
+        $filter->{early}->{-or} = [{rga_primary => 1}, {rgt_primary => 1}];
+        $c->forward('/tapper/reports/prepare_this_weeks_reportlists', [ $filter_condition ]);
+
+}
+
+sub prepare_this_weeks_reportlists : Private
+{
+        my ( $self, $c, $filter_condition ) = @_;
+
+        my @this_weeks_reportlists : Stash = ();
+        my $requested_day          : Stash;
+        my $days                   : Stash = $filter_condition->{days};
+        my $date                   : Stash = $filter_condition->{date};
+
+        $filter_condition->{early} =  {} unless
+          defined($filter_condition->{early}) and
+            ref($filter_condition->{early}) eq 'HASH' ;
+
+        # how long is "last weeks"
+        my $lastday = $filter_condition->{days} ? $filter_condition->{days} - 1 : 6;
+
+        # ----- general -----
+
+        # Mnemonic: rga = ReportGroupArbitrary, rgt = ReportGroupTestrun
+        my $reports = $c->model('ReportsDB')->resultset('Report')->search
+            (
+             $filter_condition->{early},
+             {  order_by  => 'me.id desc',
+                columns   => [ qw( id
+                                   machine_name
+                                   created_at
+                                   success_ratio
+                                   successgrade
+                                   reviewed_successgrade
+                                   total
+                                   peerport
+                                   peeraddr
+                                   peerhost
+                                )],
+                join      => [ 'reportgrouparbitrary',              'reportgrouptestrun', 'suite' ],
+                '+select' => [ 'reportgrouparbitrary.arbitrary_id', 'reportgrouparbitrary.primaryreport', 'reportgrouptestrun.testrun_id', 'reportgrouptestrun.primaryreport', 'suite.id', 'suite.name', 'suite.type', 'suite.description' ],
+                '+as'     => [ 'rga_id',                            'rga_primary',                        'rgt_id',                        'rgt_primary',                      'suite_id', 'suite_name', 'suite_type', 'suite_description' ],
+             }
+            );
+        foreach my $filter (@{$filter_condition->{late}}) {
+                $reports = $reports->search($filter);
+        }
+
+
+        my $util_report = Tapper::Reports::Web::Util::Report->new();
+
+        my @day    = ( $requested_day );
+        push @day, $requested_day->clone->subtract( days => $_ ) foreach 1..$lastday;
+
+        # ----- today -----
+        my $day0_reports = $reports->search ( { created_at => { '>', $day[0] } } );
+        push @this_weeks_reportlists, {
+                                       day => $day[0],
+                                       %{ $util_report->prepare_simple_reportlist($c, $day0_reports) }
+                                      };
+
+        # ----- last week days -----
+        foreach (1..$lastday) {
+                my $day_reports = $reports->search ({ -and => [ created_at => { '>', $day[$_]     },
+                                                                created_at => { '<', $day[$_ - 1] },
+                                                              ]});
+                push @this_weeks_reportlists, {
+                                               day => $day[$_],
+                                               %{ $util_report->prepare_simple_reportlist($c, $day_reports) }
+                                              };
+        }
+
+
+        my $list_count_all     : Stash = 0;
+        my $list_count_pass    : Stash = 0;
+        my $list_count_fail    : Stash = 0;
+        my $list_count_unknown : Stash = 0;
+
+        foreach (0..$lastday) {
+                my $reportlist = $this_weeks_reportlists[$_];
+                $list_count_all += @{$reportlist->{all_reports}};
+                foreach my $report (@{$reportlist->{all_reports}}) {
+                        if    ($report->{successgrade} eq 'PASS') { $list_count_pass++    }
+                        elsif ($report->{successgrade} eq 'FAIL') { $list_count_fail++    }
+                        else                                      { $list_count_unknown++ }
+                }
+        }
+}
+
+sub prepare_filter_path
+{
+        my ($self, $c, $days) = @_;
+        my %args = @{$c->req->arguments};
+
+        $args{days} = $days if $days;
+
+        return join('/', %args );
+}
+
+sub prepare_navi : Private
+{
+        my ( $self, $c ) = @_;
+        my $navi : Stash = [];
+
+        my %args = @{$c->req->arguments};
+        return [] if grep { /^date$/ } keys %args;
+
+        $navi = [
+                 {
+                  title  => "reports by date",
+                  href   => "/tapper/overview/date",
+                  subnavi => [
+                              {
+                               title  => "today",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 1),
+                              },
+                              {
+                               title  => "2 days",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 2),
+                              },
+                              {
+                               title  => "1 week",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 7),
+                              },
+                              {
+                               title  => "2 weeks",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 14),
+                              },
+                              {
+                               title  => "3 weeks",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 21),
+                              },
+                              {
+                               title  => "1 month",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 31),
+                              },
+                              {
+                               title  => "2 months",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 62),
+                              },
+                              {
+                               title  => "4 months",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 124),
+                              },
+                              {
+                               title  => "6 months",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 182),
+                              },
+                              {
+                               title  => "12 months",
+                               href   => "/tapper/reports/".$self->prepare_filter_path($c, 365),
+                              },
+
+                             ],
+                 },
+                 {
+                  title  => "reports by suite",
+                  href   => "/tapper/overview/suite",
+                 },
+                 {
+                  title  => "reports by host",
+                  href   => "/tapper/overview/host",
+                 },
+                 {
+                  title  => "This list as RSS",
+                  href   => "/tapper/rss/".$self->prepare_filter_path($c),
+                  image  => "/tapper/static/images/rss.png",
+                 }
+
+                 # {
+                 #  title  => "reports by people",
+                 #  href   => "/tapper/reports/people/",
+                 #  active => 0,
+                 # },
+                ];
+
+}
+
+1;
