dc1da3187cb2ee9413277a8fd157de6f10a45131
Maik Hentsche <maik.hentsche@amd.com>
2011-05-26 10:49:56 +0200

    Splitted filter into multiple modules
    Filters in testrun and reports listing have much in common but some
    parts are also very different. Thus, it makes sense to have to separate
    modules for generating these filters that inherit common code from a
    third one. This is what we have now. To make the complexity of which
    functions are callable easier to handle we now have a dispatch attribute
    that contains links from filter keys to the associated handler
    function.

--- lib/Tapper/Reports/Web/Util/Filter.pm
+++ lib/Tapper/Reports/Web/Util/Filter.pm
@@ -3,17 +3,36 @@ package Tapper::Reports::Web::Util::Filter;
 use DateTime;
 use Data::Dumper;
 use DateTime::Format::DateParse;
+use Hash::Merge::Simple 'merge';
 
 use Moose;
 
+
+
 use common::sense;
 
-has 'context' => (is => 'rw');
+has 'context'         => (is => 'rw');
 has 'requested_day'   => (is => 'rw');
+has 'dispatch'        => (is        => 'rw',
+                          isa       => 'HashRef',
+                          # builder   => 'build_dispatch',
+                          # init_arg  => undef,
+                         );
+
+sub BUILD{
+        my $self = shift;
+        my $args = shift;
+
+        $self->dispatch(
+                        merge($self->dispatch, {days => \&days})
+                       );
+}
+
 
 sub days
 {
         my ($self, $filter_condition, $days) = @_;
+
         if (defined($self->requested_day)) {
                 push @{$filter_condition->{error}}, "Time filter already exists, only using first one";
                 return $filter_condition;
@@ -28,77 +47,6 @@ sub days
         return $filter_condition;
 }
 
-sub host
-{
-        my ($self, $filter_condition, $host) = @_;
-        my @hosts;
-        @hosts = @{$filter_condition->{early}->{machine_name}->{in}} if $filter_condition->{early}->{machine_name};
-        push @hosts, $host;
-        $filter_condition->{early}->{machine_name} = {'in' => \@hosts};
-        return $filter_condition;
-}
-
-sub suite
-{
-        my ($self, $filter_condition, $suite) = @_;
-        my $suite_id;
-        if ($suite =~/^\d+$/) {
-                $suite_id = $suite;
-        } else {
-                my $suite_rs = $self->context->model('ReportsDB')->resultset('Suite')->search({name => $suite});
-                $suite_id = $suite_rs->first->id if $suite_rs->count;
-        }
-
-        my @suites;
-        @suites = @{$filter_condition->{early}->{suite_id}->{in}} if $filter_condition->{suite_id};
-        push @suites, $suite_id;
-
-        $filter_condition->{early}->{suite_id} = {'in' => \@suites};
-        return $filter_condition;
-}
-
-sub success
-{
-        my ($self, $filter_condition, $success) = @_;
-        if ($success =~/^\d+$/) {
-                $filter_condition->{early}->{success_ratio} = int($success);
-        } else {
-                $filter_condition->{early}->{successgrade} = uc($success);
-        }
-        return $filter_condition;
-
-}
-
-sub date
-{
-        my ($self, $filter_condition, $date) = @_;
-        return $self->days($filter_condition, $date) if $date =~m/^\d+$/; # handle old date links correctly
-
-        if (defined($self->requested_day)) {
-                push @{$filter_condition->{error}}, "Time filter already exists, only using first one";
-                return $filter_condition;
-        }
-
-        $filter_condition->{days} = 1;
-
-        my $requested_day;
-        my $one_day_later;
-        eval {
-                $requested_day = DateTime::Format::DateParse->parse_datetime( $date );
-                $one_day_later = DateTime::Format::DateParse->parse_datetime( $date )->add(days => 1);
-        };
-        if (not defined $requested_day) {
-                push @{$filter_condition->{error}}, "Can not parse date '$date'";
-                return $filter_condition;
-        }
-
-        $filter_condition->{date} = $requested_day->ymd('/');
-        $self->requested_day($requested_day);
-        push @{$filter_condition->{late}}, {created_at => {'>=' => $requested_day}};
-        push @{$filter_condition->{late}}, {created_at => {'<'  => $one_day_later}};
-        return $filter_condition;
-}
-
 =head2 parse_filters
 
 Parse filter arguments given by controller. The function expects an
@@ -110,14 +58,15 @@ The second argument is a list of word that are valid filters.
 
 The function returns a hash ref containing the following elements (not
 necessarily all every time):
-* early - hash ref  - contains the filter conditions that can be given
-                      in the initial search on reports
-* late  - array ref - list of hash refs that contain filter conditions
-                      which shall be applied as $report->search{$key => $value}
-                      after the initial search returned a$report already
-* error - array ref - contains all errors that occured as strings
-* days  - int       - number of days that will be shown
-* date  - string    - the exact date that will be shown
+* early    - hash ref  - contains the filter conditions that can be given
+                         in the initial search on reports
+* late     - array ref - list of hash refs that contain filter conditions
+                         which shall be applied as $report->search{$key => $value}
+                         after the initial search returned a$report already
+* function - hash ref  - functions that are to be applied as $report->$key($value);
+* error    - array ref - contains all errors that occured as strings
+* days     - int       - number of days that will be shown
+* date     - string    - the exact date that will be shown
 
 
 @param array ref - list of filter arguments
@@ -152,19 +101,16 @@ sub parse_filters
                                 return $filter_condition;
                         }
 
-                        given ($key){
-                                when ('days')    { $filter_condition = $self->days($filter_condition, $value) }
-                                when ('date')    { $filter_condition = $self->date($filter_condition, $value) }
-                                when ('host')    { $filter_condition = $self->host($filter_condition, $value) }
-                                when ('suite')   { $filter_condition = $self->suite($filter_condition, $value) }
-                                when ('success') { $filter_condition = $self->success($filter_condition, $value) }
-                                default {my @errors = @{$filter_condition->{error} || [] };
-                                         $filter_condition = {};
-                                         push @errors, "Can not parse filter $key/$value. No filters applied";
-                                         $filter_condition->{error} = \@errors;
-                                         return $filter_condition;
-                                 }
+                        if (not $self->dispatch->{$key}) {
+                                my @errors = @{$filter_condition->{error} || [] };
+                                $filter_condition = {};
+                                push @errors, "Can not parse filter $key/$value. No filters applied";
+                                $filter_condition->{error} = \@errors;
+                                return $filter_condition;
                         }
+                        use Data::Dumper;
+                        print STDERR Dumper $filter_condition;
+                        $filter_condition = $self->dispatch->{$key}->($self, $filter_condition, $value);
                 }
         }
         return $filter_condition;
---
+++ lib/Tapper/Reports/Web/Util/Filter/Report.pm
@@ -0,0 +1,161 @@
+package Tapper::Reports::Web::Util::Filter::Report;
+
+
+=head1 NAME
+
+Tapper::Reports::Web::Util::Filter::Report - Filter utilities for report listing
+
+=head1 SYNOPSIS
+
+ use Tapper::Reports::Web::Util::Filter::Report;
+ my $filter              = Tapper::Reports::Web::Util::Filter::Report->new();
+ my $filter_args         = ['host','bullock','days','3'];
+ my $allowed_filter_keys = ['host','days'];
+ my $searchoptions       = $filter->parse_filters($filter_args, $allowed_filter_keys);
+
+=cut
+
+
+
+use Moose;
+use Hash::Merge::Simple 'merge';
+
+extends 'Tapper::Reports::Web::Util::Filter';
+
+sub BUILD{
+        my $self = shift;
+        my $args = shift;
+
+        $self->dispatch(
+                        merge($self->dispatch,
+                              {host    => \&host,
+                               suite   => \&suite,
+                               success => \&success,
+                               date    => \&date,
+                              })
+                       );
+}
+
+
+=head2 host
+
+Add host filters to early filters.
+
+@param hash ref - current version of filters
+@param string   - host name
+
+@return hash ref - updated filters
+
+=cut
+
+sub host
+{
+        my ($self, $filter_condition, $host) = @_;
+        my @hosts;
+        @hosts = @{$filter_condition->{early}->{machine_name}->{in}} if $filter_condition->{early}->{machine_name};
+        push @hosts, $host;
+        $filter_condition->{early}->{machine_name} = {'in' => \@hosts};
+        return $filter_condition;
+}
+
+=head2 suite
+
+Add test suite to early filters.
+
+@param hash ref - current version of filters
+@param string   - suite name or id
+
+@return hash ref - updated filters
+
+=cut
+
+sub suite
+{
+        my ($self, $filter_condition, $suite) = @_;
+        my $suite_id;
+        if ($suite =~/^\d+$/) {
+                $suite_id = $suite;
+        } else {
+                my $suite_rs = $self->context->model('ReportsDB')->resultset('Suite')->search({name => $suite});
+                $suite_id = $suite_rs->first->id if $suite_rs->count;
+        }
+
+        my @suites;
+        @suites = @{$filter_condition->{early}->{suite_id}->{in}} if $filter_condition->{suite_id};
+        push @suites, $suite_id;
+
+        $filter_condition->{early}->{suite_id} = {'in' => \@suites};
+        return $filter_condition;
+}
+
+=head2 success
+
+Add success filters to early filters. Valid values are pass, fail and a
+ratio in percent.
+
+@param hash ref - current version of filters
+@param string   - success grade
+
+@return hash ref - updated filters
+
+=cut
+
+sub success
+{
+        my ($self, $filter_condition, $success) = @_;
+        if ($success =~/^\d+$/) {
+                $filter_condition->{early}->{success_ratio} = int($success);
+        } else {
+                $filter_condition->{early}->{successgrade} = uc($success);
+        }
+        return $filter_condition;
+
+}
+
+=head2 date
+
+Add a date to late filters. Furthermore, it sets the days value. Date
+used to filter for the number of days (now provided by function
+'days'). For backwards compatibility it checks whether the input to mean
+day rather than days and forwards accordingly.
+
+@param hash ref - current version of filters
+@param string   - date
+
+@return hash ref - updated filters
+
+=cut
+
+
+sub date
+{
+        my ($self, $filter_condition, $date) = @_;
+        return $self->days($filter_condition, $date) if $date =~m/^\d+$/; # handle old date links correctly
+
+        if (defined($self->requested_day)) {
+                push @{$filter_condition->{error}}, "Time filter already exists, only using first one";
+                return $filter_condition;
+        }
+
+        $filter_condition->{days} = 1;
+
+        my $requested_day;
+        my $one_day_later;
+        eval {
+                $requested_day = DateTime::Format::DateParse->parse_datetime( $date );
+                $one_day_later = DateTime::Format::DateParse->parse_datetime( $date )->add(days => 1);
+        };
+        if (not defined $requested_day) {
+                push @{$filter_condition->{error}}, "Can not parse date '$date'";
+                return $filter_condition;
+        }
+
+        $filter_condition->{date} = $requested_day->ymd('/');
+        $self->requested_day($requested_day);
+        push @{$filter_condition->{late}}, {created_at => {'>=' => $requested_day}};
+        push @{$filter_condition->{late}}, {created_at => {'<'  => $one_day_later}};
+        return $filter_condition;
+}
+
+
+1;
