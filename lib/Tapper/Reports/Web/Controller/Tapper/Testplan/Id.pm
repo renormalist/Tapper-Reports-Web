e8dcf6779165e1f44ca9772940e502181bf450aa
Steffen Schwigon <steffen.schwigon@amd.com>
2011-11-10 10:03:45 +0100

    more details searched from reports
    We now have more elaborate DPaths to
    look into typical places of test reports
    where the used image/kernel/test is
    reported.
    
    This combines typical baremetal and virtualized
    scenarios.

--- lib/Tapper/Reports/Web/Controller/Tapper/Testplan/Id.pm
+++ lib/Tapper/Reports/Web/Controller/Tapper/Testplan/Id.pm
@@ -26,10 +26,16 @@ sub parse_testrun
         my ($self, $testrun) = @_;
         my $description = $testrun->{description};
         my %testrun;
-        $testrun{image}     = $description ~~ dpath '/preconditions/*/mount[value eq "/"]/../image';
-        $testrun{kernel}    = $description ~~ dpath '/preconditions/*/filename[ value =~ /linux-.*2.6/]';
-        $testrun{test}      = [ map { basename($_) } @{$description ~~
-                                                         dpath '/preconditions/*/precondition_type[ value eq "testprogram"]/../program'} ];
+
+        # several places for the root system
+        push @{$testrun{image}}, @{ $description ~~ dpath '/preconditions/*/mount[value eq "/"]/../image' };
+        push @{$testrun{image}}, @{ $description ~~ dpath '//root/precondition_type[value eq "autoinstall"]/../name' };
+        push @{$testrun{image}}, @{ $description ~~ dpath '//precondition_type[value eq "autoinstall"]/../name' };
+
+        push @{$testrun{kernel}}, @{$description ~~ dpath '/preconditions/*/filename[ value =~ /linux-.*\d+.\d+/]'};
+        push @{$testrun{test}},
+         map { basename($_) }
+          @{$description ~~ dpath '/preconditions/*/precondition_type[ value eq "testprogram"]/../program'};
         $testrun{shortname} = $description->{shortname};
         return \%testrun;
 }
